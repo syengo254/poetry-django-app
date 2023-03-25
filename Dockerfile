5173d6aab051c7a8743573f032ee4938d1cfeae4
David Syengo <d.syengo@institute.global>
2023-03-25 12:09:56 +0300

    feat: added docker and compose files

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.10.2
+
+ENV PYTHONBUFFERED 1 \
+    POETRY_VIRTUALENVS_CREATE=false \
+    POETRY_CACHE_DIR='/var/cache/pypoetry'
+
+WORKDIR /code
+
+COPY poetry.lock pyproject.toml /code/
+
+RUN pip3 install poetry
+
+RUN poetry install -n --no-ansi
+
+COPY . .
--- poetrydjango/settings.py
+++ poetrydjango/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,12 +75,10 @@ WSGI_APPLICATION = 'poetrydjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {"default": dj_database_url.config(
+    conn_max_age=60,
+    conn_health_checks=True,
+)}
 
 
 # Password validation
